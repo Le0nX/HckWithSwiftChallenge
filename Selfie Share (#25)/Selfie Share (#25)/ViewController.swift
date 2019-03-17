48d2beb4cb3ec876de84e3b29a2d035a80cc99a8
Le0nX <cloudmindone@gmail.com>
2019-03-17 17:55:00 +0300

    init: added new project

---
+++ Selfie Share (#25)/Selfie Share (#25)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfie Share (#25)
+//
+//  Created by Denis Nefedov on 17/03/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
